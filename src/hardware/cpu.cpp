133229aafc7c9b3e9f4976a870ff1fa09c1c9af0
Siddha Kilaru <siddhak@nagini-144.dynamic.rpi.edu>
2022-03-18 17:08:49 -0400

    add handler

--- src/hardware/cpu.cpp
+++ src/hardware/cpu.cpp
@@ -1,10 +1,30 @@
 #include "cpu.h"
 
-cpu::cpu() 
-	: m_registers{}
-	, m_stack{} {
+const cpu::instruction_handler cpu::s_handlers[4][8] = {
+    {
+    },
+    {
+        &cpu::ora,
+        &cpu::and,
+    },
+    {
+
+    },
+    {
+
+    }
+};
+
+cpu::cpu(const std::vector<u8> &data) 
+    : m_registers{}
+    , m_stack{}
+    , m_code(data) {
+        reset();
+
+    //u8 opcode=0;
+    //(this->*s_handlers[opcode & 0xff][opcode >> 20])((opcode >> 8) & 0xfff);
 }
 
 void cpu::reset() {
-	m_registers.S = stack_size - 1; // $FF
+    m_registers.S = stack_size - 1; // $FF
 }
