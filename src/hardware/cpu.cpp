74d1ffdd0978770782b2b9470b75ef5c08f092d5
ariddl <watchoutmayhem@gmail.com>
2022-03-18 19:11:44 -0400

    cpu instruction stubs

--- src/hardware/cpu.cpp
+++ src/hardware/cpu.cpp
@@ -32,6 +32,7 @@ const cpu::instruction_handler cpu::s_handlers[4][8] = {
 		&cpu::inc
     },
     {
+		// TODO: NES Specific instructions?
 		nullptr,
 		nullptr,
 		nullptr,
@@ -43,22 +44,109 @@ const cpu::instruction_handler cpu::s_handlers[4][8] = {
     }
 };
 
-bool cpu::ora(int addrmode)
-{
-
-}
-
-
 cpu::cpu(const std::vector<u8> &data) 
     : m_registers{}
     , m_stack{}
     , m_code(data) {
-        reset();
-
-    //u8 opcode=0;
-    //(this->*s_handlers[opcode & 0xff][opcode >> 20])((opcode >> 8) & 0xfff);
+    
+	reset();
 }
 
 void cpu::reset() {
     m_registers.S = stack_size - 1; // $FF
+}
+
+bool cpu::bit(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::jmp(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::jmp_abs(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::sty(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::ldy(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::cpy(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::cpx(int addrmode) {
+	return false; // stub
+}
+
+// cc == 01
+bool cpu::ora(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::and(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::eor(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::adc(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::sta(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::lda(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::cmp(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::sbc(int addrmode) {
+	return false; // stub
+}
+
+
+// cc == 10
+bool cpu::asl(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::rol(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::lsr(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::ror(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::stx(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::ldx(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::dec(int addrmode) {
+	return false; // stub
+}
+
+bool cpu::inc(int addrmode) {
+	return false; // stub
 }
